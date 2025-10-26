b889edb8e522d155881bdfe97ef9bbcc41e2fefc
Bima Adnandita <bima.adnandita@gmail.com>
2025-10-26 13:55:52 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/public";
+import Books from "./pages/public/books";
+import PublicLayout from "./layouts/public";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import AdminLayout from "./layouts/admin";
+import Dashboard from "./pages/admin";
+import AdminBooks from "./pages/admin/books";
+import AdminGenres from "./pages/admin/genres";
+import BookCreate from "./pages/admin/books/create";
+import GenreCreate from "./pages/admin/genres/create";
+import AdminAuthors from "./pages/admin/authors";
+import AuthorCreate from "./pages/admin/authors/create";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PublicLayout />}>
+            <Route index element={<Home />} />
+            <Route path="books" element={<Books />} />
+          </Route>
+
+          <Route path="login" element={<Login/>} />
+          <Route path="register" element={<Register/>} />
+
+          <Route path="admin" element={<AdminLayout/>}>
+            <Route index element={<Dashboard/>} />
+
+            <Route path="books">
+              <Route index element={<AdminBooks/>} />
+              <Route path="create" element={<BookCreate/>}/>
+            </Route>
+
+            <Route path="genres">
+              <Route index element={<AdminGenres/>} />
+              <Route path="create" element={<GenreCreate/>}/>
+            </Route>
+
+            <Route path="authors">
+              <Route index element={<AdminAuthors/>} />
+              <Route path="create" element={<AuthorCreate/>}/>
+            </Route>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/layouts/public.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+export default function PublicLayout() {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    )
+}
